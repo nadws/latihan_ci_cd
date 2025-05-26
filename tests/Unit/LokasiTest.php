596ac218161b0504ca8c293b4059aef1bf84d15a
nadws <nandw567@gmail.com>
2025-05-26 10:04:24 +0800

    add lokasi tes

---
+++ tests/Unit/LokasiTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class LokasiTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+    public function test_lokasi_unit_fail()
+    {
+        $this->assertTrue(false); // Ini dijamin gagal
+    }
+}
